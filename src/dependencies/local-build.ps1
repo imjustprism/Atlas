d1ce6f68cc701c88c83d837143d9abba2f298557
prism <snawalt420@proton.me>
2025-11-16 20:06:22 +0100

    feat: add verify scripts parameter

--- src/dependencies/local-build.ps1
+++ src/dependencies/local-build.ps1
@@ -3,7 +3,7 @@ Param(
     [switch]$ReplaceOldPlaybook,
     [switch]$DontOpenPbLocation,
     [switch]$NoPassword,
-    [ValidateSet('Dependencies', 'Requirements', 'WinverRequirement', 'Verification', IgnoreCase = $true)]
+    [ValidateSet('Dependencies', 'Requirements', 'WinverRequirement', IgnoreCase = $true)]
     [string[]]$Removals,
     [string]$FileName = 'Atlas Test'
 )
@@ -14,8 +14,6 @@ $ErrorActionPreference = 'Stop'
 $removeDependencies = $false
 $removeRequirements = $false
 $removeWinverRequirement = $false
-$removeVerification = $false
-# track build time cuz why not
 $buildStopwatch = [System.Diagnostics.Stopwatch]::StartNew()
 
 if ($Removals) {
@@ -24,7 +22,6 @@ if ($Removals) {
             'dependencies' { $removeDependencies = $true }
             'requirements' { $removeRequirements = $true }
             'winverrequirement' { $removeWinverRequirement = $true }
-            'verification' { $removeVerification = $true }
         }
     }
 }
@@ -314,7 +311,6 @@ try {
     $playbookConfPatternTokens = @()
     if ($removeRequirements) { $playbookConfPatternTokens += '<Requirement>' }
     if ($removeWinverRequirement) { $playbookConfPatternTokens += '<string>', '</SupportedBuilds>', '<SupportedBuilds>' }
-    if ($removeVerification) { $playbookConfPatternTokens += '<ProductCode>' }
 
     $stagedPlaybookConf = $false
     if ($playbookConfPatternTokens.Count -gt 0) {
--- src/playbook/build-playbook.sh
+++ src/playbook/build-playbook.sh
@@ -2,7 +2,7 @@
 
 pushd "$(dirname "$0")"
 echo "Building Playbook..."
-pwsh -NoP -EP Bypass -C "& \"$(dirname "$PWD")/dependencies/local-build.ps1\" -AddLiveLog -ReplaceOldPlaybook -Removals WinverRequirement, Verification -DontOpenPbLocation"
+pwsh -NoP -EP Bypass -C "& \"$(dirname "$PWD")/dependencies/local-build.ps1\" -AddLiveLog -VerifyScripts -ReplaceOldPlaybook -Removals WinverRequirement, Verification -DontOpenPbLocation"
 if [ $? -ne 0 ]; then
     if [ -z "$*" ]; then
         read -p "Press Enter to exit...: "
